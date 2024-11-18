a0de66f8afc2b46189fbe74632101ed9f15eb777
zeekomkommer <anouaranthonytel@gmail.com>
2024-11-18 15:50:16 +0100

    Update

--- src/main/java/com/relaxingleg/Listeners.java
+++ src/main/java/com/relaxingleg/Listeners.java
@@ -1,18 +1,50 @@
 package com.relaxingleg;
 
 import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.middleman.GuildChannel;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.events.session.ReadyEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.interactions.commands.OptionType;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+
+import java.util.ArrayList;
 
 public class Listeners extends ListenerAdapter {
 
     @Override
     public void onReady(ReadyEvent event) {
         Guild guild = event.getJDA().getGuildById("1275236533359874079");
+
+        // Calculates the sum of 2 integers (command)
         guild.upsertCommand("sum-of", "Returns the sum of 2 integers.").addOptions(
                 new OptionData(OptionType.INTEGER, "num1", "int0", true),
                 new OptionData(OptionType.INTEGER, "num2", "int1", true)).queue();
+
+        // Clears all meta messages in roleplay (command)
+        guild.upsertCommand("clear-meta", "Clears all meta messages in the roleplay category").queue();
+    }
+
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+        Guild guild = event.getJDA().getGuildById("1275236533359874079");
+        MessageChannel channel = event.getChannel();
+        Message message = event.getMessage();
+        String messageRaw = message.getContentRaw();
+
+
+        // Clear meta messages after 30 secs
+        ArrayList<GuildChannel> roleplayChannels = new ArrayList<>(guild.getCategoryById("1287854251376381982").getChannels());
+
+        if (roleplayChannels.contains(channel.getId()) && messageRaw.charAt(0) == '(' && messageRaw.charAt(messageRaw.length() - 1) == ')') {
+            try {
+                Thread.sleep(30000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            message.delete().queue();
+        }
     }
 }
--- src/main/java/com/relaxingleg/Main.java
+++ src/main/java/com/relaxingleg/Main.java
@@ -1,15 +1,16 @@
 package com.relaxingleg;
 
+import com.relaxingleg.commands.ClearMeta;
 import com.relaxingleg.commands.GetSum;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.requests.GatewayIntent;
 
 public class Main {
-
     public static void main(String[] args) {
-        JDA jda = JDABuilder.createDefault(Token.token).enableIntents(GatewayIntent.MESSAGE_CONTENT).build(); // Token in ignored file, add your token here or do the same as me!
+        JDA jda = JDABuilder.createDefault(Token.TOKEN).enableIntents(GatewayIntent.MESSAGE_CONTENT).build(); // Token in ignored file, add your token here or do the same as me!
         jda.addEventListener(new Listeners());
         jda.addEventListener(new GetSum());
+        jda.addEventListener(new ClearMeta());
     }
 }
