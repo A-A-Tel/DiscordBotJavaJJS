256da8f62c6ceb40ebe67fa21f78c568d0a9ae27
zeekomkommer <anouaranthonytel@gmail.com>
2024-11-21 22:24:44 +0100

    Made unkillable

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+import subprocess
 from time import sleep
 
 import psutil
 
+
+def find_bot():
+    for proc in psutil.process_iter(['pid', 'name', 'cmdline']):
+        try:
+            if 'java' in proc.info['name'] and "Bot.jar" in ' '.join(proc.info['cmdline']):
+                return True
+        except (psutil.NoSuchProcess, psutil.AccessDenied):
+            continue
+    return False
+
+
 while True:
-    for process in psutil.process_iter():
-        if process.name == "Bot-1.0-all.jar":
-            print(process.name)
-    sleep(5)
+    if not find_bot():
+        ran = subprocess.run(["java", "-jar", "D:/Personal/Discord/DiscordBotJavaJJS/build/libs/Bot.jar"], capture_output=True, text=True)
+
+        print(ran)
+    else:
+        print("found")
+    sleep(10)
