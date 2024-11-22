fdd4b74fdb9d95d266cdebd8d4f7e0147be586bb
zeekomkommer <anouaranthonytel@gmail.com>
2024-11-22 20:55:08 +0100

    Update TicTacToe.java

--- src/main/java/com/relaxingleg/commands/TicTacToe.java
+++ src/main/java/com/relaxingleg/commands/TicTacToe.java
@@ -1,9 +1,13 @@
 package com.relaxingleg.commands;
 
 import com.relaxingleg.ICommand;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.channel.ChannelType;
+import net.dv8tion.jda.api.entities.channel.middleman.MessageChannel;
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
 import net.dv8tion.jda.api.interactions.commands.build.OptionData;
+import okhttp3.internal.ws.RealWebSocket;
 
 import java.util.List;
 
@@ -30,9 +34,18 @@ public class TicTacToe implements ICommand {
 
     @Override
     public void execute(SlashCommandInteractionEvent event) {
-        event.getChannel().sendMessage("""
+        MessageChannel channel = event.getChannel();
+        channel.sendMessage("""
+                
                 1️⃣2️⃣3️⃣
                 4️⃣5️⃣6️⃣
                 7️⃣8️⃣9️⃣""").queue();
+        long messageId = channel.getLatestMessageIdLong();
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+        channel.editMessageById(messageId, "Ballala").queue();
     }
 }
